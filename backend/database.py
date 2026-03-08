2b67dfca2ff57078afd49e281c33ae7ccb65e03f
Luan <luan.sena14@gmail.com>
2026-03-08 17:54:46 -0300

    trocando database

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,27 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv
+# from urllib.parse import quote_plus
+# import os
+
+# load_dotenv(encoding='utf-8')
+
+# password = os.getenv("DB_PASSWORD")
+
+# # quote_plus escapa caracteres especiais como @ % # automaticamente
+# DATABASE_URL = f"postgresql://postgres:{quote_plus(password)}@localhost:5432/bodylog"
+
+# engine = create_engine(DATABASE_URL)
+# Session = sessionmaker(bind=engine)
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-from urllib.parse import quote_plus
-import os
 
-load_dotenv(encoding='utf-8')
+DATABASE_URL = "sqlite:///./database.db"
 
-password = os.getenv("DB_PASSWORD")
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-# quote_plus escapa caracteres especiais como @ % # automaticamente
-DATABASE_URL = f"postgresql://postgres:{quote_plus(password)}@localhost:5432/bodylog"
-
-engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
