4525f8c6f756e70b5893b271b17c2156884b1b82
Luan <luan.sena14@gmail.com>
2026-03-09 12:11:15 -0300

    corrigindo database

--- backend/database.py
+++ backend/database.py
@@ -1,27 +1,20 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from dotenv import load_dotenv
-# from urllib.parse import quote_plus
-# import os
-
-# load_dotenv(encoding='utf-8')
-
-# password = os.getenv("DB_PASSWORD")
-
-# # quote_plus escapa caracteres especiais como @ % # automaticamente
-# DATABASE_URL = f"postgresql://postgres:{quote_plus(password)}@localhost:5432/bodylog"
-
-# engine = create_engine(DATABASE_URL)
-# Session = sessionmaker(bind=engine)
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# compatibilidade com render
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
 
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
